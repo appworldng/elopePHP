ccdb4817d31d2be8da29febc7e235cb9bdf4ed56
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-27 10:17:20 +0100

    Update master model class

---
+++ models/model.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Filename:      model.php
+ * Framework:     easyPHP
+ * Framework URI: https://github.com/chigozieorunta/easyPHP
+ * Domain Path:   /models
+ * Description:   The Parent(master) model class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+class Model {
+
+}
+
+?>
