4cacaa2641a90cbe27970cc327397df2e8630a13
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-27 09:03:46 +0100

    Database class definition

---
+++ config/database.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Filename:      database.php
+ * Framework:     easyPHP
+ * Framework URI: https://github.com/chigozieorunta/easyPHP
+ * Domain Path:   /config
+ * Description:   The database class with a singleton pattern for instantiation
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+ /**
+ * Class Database
+ */
+class Database {
+    /**
+	 * Private static variables
+	 *
+	 * @var string
+	 */
+    private static $localhost;
+    private static $username;
+    private static $password;
+    private static $database;
+
+    /**
+	 * Public static variables
+	 *
+	 * @var string
+	 */
+    public static $connectionID;
+    public static $connectionErrMsg;
+}
+
+?>
---
+++ config/database1.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Filename:      database.php
+ * Framework:     easyPHP
+ * Framework URI: https://github.com/chigozieorunta/easyPHP
+ * Domain Path:   /config
+ * Description:   The database class with a singleton pattern for instantiation
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+Database::getInstance();
+
+ /**
+ * Class Database
+ */
+class Database {
+    /**
+	 * Private static variables
+	 *
+	 * @var string
+	 */
+    private static $localhost;
+    private static $username;
+    private static $password;
+    private static $database;
+
+    /**
+	 * Public static variables
+	 *
+	 * @var string
+	 */
+    public static $connectionID;
+    public static $connectionErrMsg;
+
+    /**
+	 * Constructor
+	 *
+	 * @since  1.0.0
+	 */
+    public function __construct() {
+        self::$localhost = '';
+        self::$username = '';
+        self::$password = '';
+        self::$database = '';
+        self::connect();
+    }
+
+    /**
+	 * Points the class, singleton.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 */
+    public static function getInstance() {
+        static $instance;
+        if($instance === null) $instance = new self();
+        return $instance;
+    }
+
+    /**
+	 * Connection class
+     * 
+	 * @access private
+	 * @since  1.0.0
+	 */
+    private static function connect() {
+        if(!self::$connectionID = 
+            mysqli_connect(
+                self::$localhost, 
+                self::$username, 
+                self::$password, 
+                self::$database
+            )
+        ) self::$connectionErrMsg = 'Unable to connect';
+    }
+}
+
+?>
