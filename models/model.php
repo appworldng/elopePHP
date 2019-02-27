b62b17da6ccefb40a4dc835352da1282217e4cfe
Chigozie Orunta <chigozieorunta@yahoo.com>
2019-02-27 14:26:51 +0100

    Added addRecord method

--- models/model.php
+++ models/model.php
@@ -42,7 +42,7 @@ class Model {
 	 * @since  1.0.0
 	 */
     public function getRecord($id) {
-        $sql = "SELECT * FROM '.$this->className.' WHERE id = $id";
+        $sql = "SELECT * FROM ".$this->className." WHERE id = $id";
         $result = mysqli_query(self::$connectionID, $sql);
         $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
         //Properties...
@@ -53,13 +53,27 @@ class Model {
     }
 
     /**
+	 * addRecord($args) Method
+     * 
+	 * @access public
+	 * @since  1.0.0
+	 */
+    public function addRecord($args) {
+        foreach($args as $key=>$value) {
+            $columns .= $key.' = '.$value;
+        }
+        $sql = "INSERT INTO ".$this->className." SET ".$columns;
+        $result = mysqli_query($this->connectionID, $sql);
+    }
+
+    /**
 	 * delRecord($id) Method
      * 
 	 * @access public
 	 * @since  1.0.0
 	 */
     public function delRecord($id) {
-        $sql = "DELETE FROM '.$this->className.' WHERE id = $id";
+        $sql = "DELETE FROM ".$this->className." WHERE id = $id";
         $result = mysqli_query($this->connectionID, $sql);
     }
 }
