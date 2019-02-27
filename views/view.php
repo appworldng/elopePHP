7cfc635b70b8e6951f5ccb72eb2ac797286fc297
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-27 11:02:56 +0100

    Modified view class

---
+++ views/view.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Filename:      view.php
+ * Framework:     elopePHP
+ * Framework URI: https://github.com/chigozieorunta/elopePHP
+ * Domain Path:   /models
+ * Description:   The Parent(master) view class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+/**
+ * Class View
+ */
+class Model {
+
+}
