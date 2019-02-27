10f242e120b8c5a0eb378bb465372940a92c7e61
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-27 11:11:40 +0100

    Controller.php Init

---
+++ controllers/controller.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Filename:      controller.php
+ * Framework:     elopePHP
+ * Framework URI: https://github.com/chigozieorunta/elopePHP
+ * Domain Path:   /controllers
+ * Description:   The Parent(master) controller class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+/**
+ * Class Controller
+ */
+class Controller {
+    /**
+	 * Constructor
+	 *
+	 * @since  1.0.0
+	 */
+    public function __construct() {}
+}
