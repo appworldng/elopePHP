16e352a52db3a424dcfe6b788edd5b1e659df08d
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-06 15:49:32 +0100

    Sample template file build

---
+++ www-templates/template.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Filename:      template.php
+ * Framework:     elopePHP
+ * Framework URI: https://github.com/chigozieorunta/elopePHP
+ * Domain Path:   /www-templates
+ * Description:   Sample HTML Template file
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/02/27
+ */
+
+ //This is a sample template file, the parent domain (www-templates) houses all your HTML templates seen in your various views...
+
+ ?>
+
+ <h1>Sample Template</h1>
